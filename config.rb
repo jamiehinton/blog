2036338644f45d32ae4c74169e61ff17a1dcdbfd
Jamie Hinton <jamie@jamiehinton.co.uk>
2012-12-14 19:44:32 +0000

    added a post and cleaned up config

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_path = "/jamiehinton/"
+http_images_path = "/jamiehinton/images"
+http_fonts_path = "/jamiehinton/fonts"
+css_dir = "public/jamiehinton/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
